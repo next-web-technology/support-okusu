5a51b6a3f110a34de707cb55cfbe116689d3e3fe
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2023-08-29 16:07:18 +0900

    build: daisyui

--- projects/admin/src/app/home/home.component.ts
+++ projects/admin/src/app/home/home.component.ts
@@ -5,7 +5,26 @@ import { CommonModule } from '@angular/common';
   selector: 'admin-home',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1 class="text-3xl font-bold underline">Hello world!</h1> `,
+  template: `
+    <div class="prose">
+      <h1>Hello world!</h1>
+      <p>
+        p tag test test test test test test test test test test test test test
+        test test test test test test
+      </p>
+      <h2>H2 Test</h2>
+      <p>
+        p tag test test test test test test test test test test test test test
+        test test test test test test
+      </p>
+      <h3>H3 Test</h3>
+      <p>
+        p tag test test test test test test test test test test test test test
+        test test test test test test
+      </p>
+      <button class="prose btn-primary w-64 rounded-full">Button</button>
+    </div>
+  `,
   styles: [],
 })
 export class HomeComponent {}
