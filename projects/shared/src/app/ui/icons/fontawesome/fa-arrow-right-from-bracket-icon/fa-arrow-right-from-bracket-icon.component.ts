ceec963149d909f04aec9297a07b812b377a2608
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2023-08-31 20:04:05 +0900

    refactor: move ui parts from web to shared

--- projects/shared/src/app/ui/icons/fontawesome/fa-arrow-right-from-bracket-icon/fa-arrow-right-from-bracket-icon.component.ts
+++ projects/shared/src/app/ui/icons/fontawesome/fa-arrow-right-from-bracket-icon/fa-arrow-right-from-bracket-icon.component.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'web-fa-user-icon',
+  selector: 'shared-fa-arrow-right-from-bracket-icon',
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   template: ` <fa-icon [icon]="iconDefinition"></fa-icon> `,
   styles: [],
 })
-export class FaUserIconComponent {
-  iconDefinition = faUser;
+export class FaArrowRightFromBracketIconComponent {
+  iconDefinition = faArrowRightFromBracket;
 }
--- projects/shared/src/app/ui/icons/fontawesome/fa-google/fa-google-icon.component.ts
+++ projects/shared/src/app/ui/icons/fontawesome/fa-google/fa-google-icon.component.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'web-fa-bars-icon',
+  selector: 'shared-fa-google-icon',
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   template: ` <fa-icon [icon]="iconDefinition"></fa-icon> `,
   styles: [],
 })
-export class FaBarsIconComponent {
-  iconDefinition = faBars;
+export class FaGoogleIconComponent {
+  iconDefinition = faGoogle;
 }
