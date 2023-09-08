ba632d9d972708c718e56024861f44d3e4cc6cdd
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2023-09-08 09:26:27 +0900

    fix: setup script

--- setup-gcp-github-actions-oidc.sh
+++ setup-gcp-github-actions-oidc.sh
@@ -7,8 +7,8 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member="serviceAccount:
 gcloud iam workload-identity-pools create "github-actions-pool" --location="global" --project="${PROJECT_ID}" --display-name="GitHub Actions Pool"
 gcloud iam workload-identity-pools providers create-oidc "github-actions-provider" --location="global" --project="${PROJECT_ID}" --workload-identity-pool="github-actions-pool" --attribute-mapping="google.subject=assertion.sub,attribute.repository=assertion.repository,attribute.actor=assertion.actor" --issuer-uri="https://token.actions.githubusercontent.com"
 gcloud iam workload-identity-pools describe "github-actions-pool" --project="${PROJECT_ID}" --location="global" --format="value(name)"
-WORKLOAD_IDENTITY_POOL_ID='PUT_ABOVE_OUTPUT_HERE'
+WORKLOAD_IDENTITY_POOL_ID=$(gcloud iam workload-identity-pools describe "github-actions-pool" --project="${PROJECT_ID}" --location="global" --format="value(name)")
 IAM_PRINCIPAL="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/*"
 gcloud iam service-accounts add-iam-policy-binding "github-actions@${PROJECT_ID}.iam.gserviceaccount.com" --project="${PROJECT_ID}" --role="roles/iam.workloadIdentityUser" --member="${IAM_PRINCIPAL}"
 gcloud iam workload-identity-pools providers describe "github-actions-provider" --project="${PROJECT_ID}" --location="global" --workload-identity-pool="github-actions-pool" --format="value(name)"
-WORKLOAD_IDENTITY_POOL_PROVIDER_ID='PUT_ABOVE_OUTPUT_HERE'
+WORKLOAD_IDENTITY_POOL_PROVIDER_ID=$(gcloud iam workload-identity-pools providers describe "github-actions-provider" --project="${PROJECT_ID}" --location="global" --workload-identity-pool="github-actions-pool" --format="value(name)")
